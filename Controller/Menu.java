206a0e00520cf54dd24e6b3ef97e8f5aac67712f
Eduard Latypov <LatypovEdik1988@yandex.ru>
2023-06-05 12:38:00 +0300

    homework complette

---
+++ Controller/Menu.java
@@ -0,0 +1,31 @@
+package Controller;
+
+public enum Menu {
+    NONE(""),
+    PLUS("+"),
+    MINUS("-"),
+    MULTYPLY("*"),
+    DIVIDE("/"),
+    EXIT("x");
+
+    private String operator;
+
+    /**
+     * Констркутор
+     * 
+     * @param operator - символ оператора
+     */
+    Menu(String operator) {
+        this.operator = operator;
+    }
+
+    /**
+     * Геттер символа оператора
+     * 
+     * @return - строковый тип (возвращает operator)
+     */
+    public String getOperator() {
+        return operator;
+    }
+
+}
